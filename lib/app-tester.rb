92053064e710ffd4684b256151df8fc8b0079607
José Airosa <j.airosa@digital-science.com>
2012-11-08 16:36:04 +0000

    Correct version number

--- lib/app-tester.rb
+++ lib/app-tester.rb
@@ -3,7 +3,7 @@ $:.unshift(File.dirname(__FILE__)) unless
 
 # @abstract AppTester main module and namespace
 module AppTester
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
 
   # @abstract AppTester main class
   # @attr_reader [AppTester::Options] Options container. This will be shared across other classes
